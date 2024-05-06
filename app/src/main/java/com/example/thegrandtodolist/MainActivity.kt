16bc6687422afe84c47f0cebe5e6f62a94acaeeb
Chris Letizio <cletizio@gmail.com>
2024-05-05 22:28:57 -0400

    Implemented SQL and got database working

--- app/src/main/java/com/example/thegrandtodolist/MainActivity.kt
+++ app/src/main/java/com/example/thegrandtodolist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.thegrandtodolist
 
+import android.content.ContentValues
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.EditText
@@ -15,16 +16,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : ComponentActivity() {
 
-    private val todoList = mutableListOf<GrandTodoList>()
+    private lateinit var todoList: MutableList<GrandTodoList>
     private lateinit var adapter: ArrayAdapter<GrandTodoList>
+    private lateinit var dbHelper: TodoDatabaseHelper
+    private lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
 
-        val listView: ListView = findViewById(R.id.todoListText)
+        dbHelper = TodoDatabaseHelper(this)
+        todoList = mutableListOf()
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todoList)
+
+        listView = findViewById(R.id.todoListText)
         listView.adapter = adapter
+
+        loadTodoItems()
 
         val editTextTodo: EditText = findViewById(R.id.inputTask)
         val buttonAddTodo: FloatingActionButton = findViewById(R.id.addTaskButton)
@@ -36,6 +44,7 @@ class MainActivity : ComponentActivity() {
                 val newTodo = GrandTodoList(todoList.size + 1, todoText)
                 todoList.add(newTodo)
                 adapter.notifyDataSetChanged()
+                saveTodoItem(newTodo)
                 editTextTodo.text.clear()
             } else{
                 Toast.makeText(this, "Please enter a task you would like to complete.",
@@ -48,6 +57,7 @@ class MainActivity : ComponentActivity() {
             // Toggle the completion status of the clicked todo item
             clickedTodo.isCompleted = !clickedTodo.isCompleted
             adapter.notifyDataSetChanged()
+            updateTodoItem(clickedTodo)
         }
 
         listView.setOnItemLongClickListener { parent, view, position, id ->
@@ -57,8 +67,49 @@ class MainActivity : ComponentActivity() {
                 this, "Deleted: ${removedTodo.todoText}",
                 Toast.LENGTH_SHORT
             ).show()
+
+            deleteTodoItem(removedTodo)
             true // Return true to consume the long click event
         }
+    }
+
+    private fun loadTodoItems() {
+        val db = dbHelper.readableDatabase
+        val cursor = db.query(TodoDatabaseHelper.TABLE_NAME, null, null, null,null,null,null)
+
+        if(cursor != null && cursor.moveToFirst()) {
+            do {
+                val todoText =
+                    cursor.getString(cursor.getColumnIndex(TodoDatabaseHelper.COLUMN_TASK))
+                val todo = GrandTodoList(0, todoText)
+                todoList.add(todo)
+            } while (cursor.moveToNext())
+        }
+        cursor?.close()
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun saveTodoItem(todo: GrandTodoList) {
+        val db = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(TodoDatabaseHelper.COLUMN_TASK, todo.todoText)
+        }
+        db.insert(TodoDatabaseHelper.TABLE_NAME, null, values)
+    }
+
+    private fun updateTodoItem(todo: GrandTodoList) {
+        val db = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(TodoDatabaseHelper.COLUMN_TASK, todo.todoText)
+        }
+        db.update(TodoDatabaseHelper.TABLE_NAME, values, "${TodoDatabaseHelper.COLUMN_TASK} " +
+                "= ?", arrayOf(todo.todoText))
+    }
+
+    private fun deleteTodoItem(todo: GrandTodoList) {
+        val db = dbHelper.writableDatabase
+        db.delete(TodoDatabaseHelper.TABLE_NAME, "${TodoDatabaseHelper.COLUMN_TASK} = ?",
+            arrayOf(todo.todoText))
     }
 }
 
---
+++ app/src/main/java/com/example/thegrandtodolist/TodoDatabaseHelper.kt
@@ -0,0 +1,28 @@
+package com.example.thegrandtodolist
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class TodoDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "TodoList.db"
+        const val TABLE_NAME = "todo_items"
+        const val COLUMN_ID = "id"
+        const val COLUMN_TASK = "task"
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        val createTableQuery = "CREATE TABLE $TABLE_NAME (" +
+                "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "$COLUMN_TASK TEXT)"
+        db.execSQL(createTableQuery)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+}
